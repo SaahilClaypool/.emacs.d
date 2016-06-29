a4a978a66b5dfc946e55841231fdb82e76067c92
Saahil Claypool <Saahil Claypool>
2016-06-29 18:49:32 -0400

    font changes

--- init.el
+++ init.el
@@ -57,17 +57,16 @@
 (setq tab-width 4)
 (setq-default indent-tabs-mode nil)
 (global-font-lock-mode 1)
-(setq-default line-spacing 4)
+(setq-default line-spacing 0)
 ;;(add-to-list 'default-frame-alist '(font . "Lucida Console"))
 ;;(set-face-attribute 'default t :font "Lucida Console" )
-;;(add-to-list 'default-frame-alist '(font . "Driod Sans Mono Regular"))
-;;(set-face-attribute 'default t :font  "Driod Sans Mono Regular")
+
 (set-face-attribute 'default (selected-frame) :height 105)
 
 ;; theme
-;;(load-theme 'monokai t)
+(load-theme 'monokai t)
 
-(load-theme 'solarized-light t)
+;;(load-theme 'solarized-light t)
 
 
 
@@ -120,7 +119,7 @@
  '(blink-cursor-mode nil)
  '(custom-safe-themes
    (quote
-    ("d737a2131d5ac01c0b2b944e0d2cb0be1c76496bb4ed61be51ff0e5457468974" "2c73700ef9c2c3aacaf4b65a7751b8627b95a1fd8cebed8aa199f2afb089a85f" "b571f92c9bfaf4a28cb64ae4b4cdbda95241cd62cf07d942be44dc8f46c491f4" default)))
+    ("5301f8ef618313dffce63c69657e58180e985958f6635c859f2c036094fd1919" "d737a2131d5ac01c0b2b944e0d2cb0be1c76496bb4ed61be51ff0e5457468974" "2c73700ef9c2c3aacaf4b65a7751b8627b95a1fd8cebed8aa199f2afb089a85f" "b571f92c9bfaf4a28cb64ae4b4cdbda95241cd62cf07d942be44dc8f46c491f4" default)))
  '(show-paren-mode t)
  '(tool-bar-mode nil))
 (custom-set-faces
